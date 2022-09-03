b0d4eef868044278f6b070d4d7e860e02564871c
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-03 00:07:54 -0300

    feat(): Criado e testado validações do formulário de registro

--- src/routes/CustomerRoutes.ts
+++ src/routes/CustomerRoutes.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config';
 import { Router } from 'express';
 import ConstrollerCustommer from '../controllers/CustomerController';
+import {
+  nameValidation,
+  emailValidation,
+  phoneNumberValidation,
+  cpfValidation,
+  addressValidation,
+} from '../middlewares/CustomerValidation';
 
 const customer = new ConstrollerCustommer();
 
@@ -8,6 +15,14 @@ const Route = Router();
 
 Route.get('/', customer.getAllCustommer);
 
-Route.post('/register', customer.createCustomer);
+Route.post(
+  '/register',
+  nameValidation,
+  emailValidation,
+  phoneNumberValidation,
+  cpfValidation,
+  addressValidation,
+  customer.createCustomer,
+);
 
 export default Route;
