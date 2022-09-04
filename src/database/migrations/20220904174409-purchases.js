a8cb318279a533d13cb3525fcd2ff381fdc7ccb6
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-04 16:04:43 -0300

    feat(): Gerado nova migration Puschases

---
+++ src/database/migrations/20220904174409-purchases.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize)
+  {
+    await queryInterface.createTable('purchases', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      customerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'customer_id',
+      },
+      treatmentId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'treatment_id',
+      },
+      paymantType: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        field: 'paymant_type',
+      },
+      purchaseDate: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'purchase_date',
+      },
+      totalInstallments: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'total_installments',
+      },
+      totalValue: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(9, 2),
+        field: 'total_value',
+      },
+    });
+  },
+
+  async down(queryInterface)
+  {
+    await queryInterface.dropTable('purchases');
+  },
+};
