b28ab6e8654d278266f6d347315503c02976020b
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-13 22:13:27 -0300

    feat(): Corrigindos erros de deploy

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app_1.app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
