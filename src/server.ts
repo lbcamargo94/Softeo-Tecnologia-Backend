17db08979d8f47eae5f719a17298262876ad05df
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-13 21:47:53 -0300

    feat(): Corrigindos erros de deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
